586ebcc48552f0e93d6b30b0cd160d526c2c832c
Sourcery AI <>
2023-01-16 07:21:19 +0000

    'Refactored by Sourcery'

--- pornhub/plugins/function.py
+++ pornhub/plugins/function.py
@@ -12,7 +12,7 @@ def humanbytes(size):
     while size > power:
         size /= power
         raised_to_pow += 1
-    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+    return f"{str(round(size, 2))} {dict_power_n[raised_to_pow]}B"
 
 
 def edit_msg(client, message, to_edit):
